bf9dd7be8102714dbb04381244cf3dff9bf97077
Ryo Kanbayashi <ryo@User-no-MacBook-Air.local>
2015-06-28 18:43:47 +0900

    add deep nn code based on kaggle otto code

---
+++ train/keras_otto_deep_nn_azure.py
@@ -0,0 +1,122 @@
+#!/usr/bin/python
+
+from __future__ import absolute_import
+from __future__ import print_function
+
+import numpy as np
+import pandas as pd
+
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation
+from keras.layers.normalization import BatchNormalization
+from keras.layers.advanced_activations import PReLU
+from keras.utils import np_utils, generic_utils
+
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
+
+np.random.seed(1337) # for reproducibility
+
+def preprocess_data(X, scaler=None):
+    if not scaler:
+        scaler = StandardScaler()
+        scaler.fit(X)
+    X = scaler.transform(X)
+    return X, scaler
+    
+def preprocess_labels(labels, encoder=None, categorical=True):
+    if not encoder:
+        encoder = LabelEncoder()
+        encoder.fit(labels)
+    y = encoder.transform(labels).astype(np.int32)
+    if categorical:
+        y = np_utils.to_categorical(y)
+    return y, encoder
+                                                                    
+def my_int(str1):
+    if "NA" in str1:
+        return 0
+    else:
+        return int(str1)
+
+def my_float(str1):
+    if "NA" in str1:
+        return 0
+    else:
+        return float(str1)    
+
+def add_converted_val(target_arr, add_arr):
+    for elem in add_arr:
+        target_arr.append(my_float(elem))
+    
+ftr_x = open('./azure_train_.csv', 'r')
+te_mat = []
+for trxline in ftr_x:
+    splited = trxline.split(",")
+    col = []
+    col.append(my_float(splited[1]))
+    add_converted_val(col, splited[4:79])
+    te_mat.append(col)
+
+print("Loading data...")
+te_np_arr = np.array(te_mat)
+X = te_np_arr[:,1:76]
+labels = te_np_arr[:,0]
+X, scaler = preprocess_data(X)
+y, encoder = preprocess_labels(labels)
+
+fts = open('./azure_test_.csv', 'r')
+enroll_ids = []
+ts_mat = []
+for tsline in fts:
+    splited = tsline.split(",")
+    enroll_ids.append(splited[0])
+    col = []
+    add_converted_val(col, splited[3:78])
+    ts_mat.append(col)
+
+X_test = np.array(ts_mat)
+X_test, _ = preprocess_data(X_test, scaler)
+
+nb_classes = y.shape[1]
+print(nb_classes, 'classes')
+
+dims = X.shape[1]
+print(dims, 'dims')
+
+print("Building model...")
+
+model = Sequential()
+model.add(Dense(dims, 512, init='glorot_uniform'))
+model.add(PReLU((512,)))
+model.add(BatchNormalization((512,)))
+model.add(Dropout(0.5))
+
+model.add(Dense(512, 512, init='glorot_uniform'))
+model.add(PReLU((512,)))
+model.add(BatchNormalization((512,)))
+model.add(Dropout(0.5))
+
+model.add(Dense(512, 512, init='glorot_uniform'))
+model.add(PReLU((512,)))
+model.add(BatchNormalization((512,)))
+model.add(Dropout(0.5))
+
+model.add(Dense(512, nb_classes, init='glorot_uniform'))
+model.add(Activation('sigmoid'))
+
+model.compile(loss='binary_crossentropy', optimizer="adam")
+
+print("Training model...")
+
+model.fit(X, y, nb_epoch=20, batch_size=128, validation_split=0.15)
+
+print("Generating submission...")
+
+proba = model.predict_proba(X_test)
+
+# print(encoder.classes_[0])
+# print(proba[0])
+frslt = open('../test/keras_otto_azure_1.csv', 'w')
+for idx in xrange(len(enroll_ids)):
+    frslt.write(enroll_ids[idx] + "," + str(encoder.classes_[idx]) + "\n")
